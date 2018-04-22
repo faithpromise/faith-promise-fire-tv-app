b8434f1736fec79557f5b53e5ad4792565a44f41
Zachary Berardo <zachary@zachnthebox.com>
2018-04-21 21:08:10 -0400

    fix: replace query param state instead of pushing to history

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  queryParams: {
+    series: {
+      replace: true
+    },
+    episode: {
+      replace: true
+    },
+  },
 });
