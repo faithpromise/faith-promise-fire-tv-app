d7fa2067f06381d40b01975cab2009f3142cc0a8
Zachary Berardo <zachary@zachnthebox.com>
2018-05-06 16:48:16 -0400

    Deployed d302189

--- assets/faith-promise-fire-tv-app.js
+++ assets/faith-promise-fire-tv-app.js
@@ -11800,6 +11800,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("faith-promise-fire-tv-app/app")["default"].create({"urls":{"currentLiveEvent":"/api/v1/events/current","onlineData":"/app-api/online","data":"/app-api/roku"},"name":"faith-promise-fire-tv-app","version":"0.0.0+e700b96a"});
+  require("faith-promise-fire-tv-app/app")["default"].create({"urls":{"currentLiveEvent":"/api/v1/events/current","onlineData":"/app-api/online","data":"/app-api/roku"},"name":"faith-promise-fire-tv-app","version":"0.0.0+d302189c"});
 }
 //# sourceMappingURL=faith-promise-fire-tv-app.map
